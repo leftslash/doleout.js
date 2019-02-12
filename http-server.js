245dccb46dbe2cd21d3aeb806016112193672cd8
Todd Castilow <todd@leftslash.com>
2019-02-12 14:14:16 -0500

    adding code

---
+++ http-server.js
@@ -0,0 +1,20 @@
+const url  = require('url')
+const http = require('http')
+
+const port = 1234
+const host = 'localhost'
+
+const server = new http.Server()
+
+server.on('request', (request, response) => {
+  // request.headers
+  // request.method
+  // request.url
+  // response.setHeader(name, value)
+  // response.statusCode 404
+  // response.statusMessage 'Not Found'
+  response.write('Hello, World!\n')
+  response.end()
+})
+
+server.listen(port, host)
